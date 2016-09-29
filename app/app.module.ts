d88f8397b6f002af63dae550f89250dfdb60f6a1
courington <chase@simplbrew.com>
2016-09-28 21:17:34 -0600

    add another basic component

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { AppTextInputComponent } from './app.textInput.component';
 
 @NgModule({
   imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap: [ AppComponent ]
+  declarations: [ AppComponent, AppTextInputComponent ],
+  bootstrap: [ AppComponent, AppTextInputComponent ]
 })
 export class AppModule { }
---
+++ app/app.textInput.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app-2',
+    template: '<input type="text"/>'
+})
+export class AppTextInputComponent { }
